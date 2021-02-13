5f2661bf428a1b845c27c21b7283372206b6ab7c
Mathew Han <mathewjhan@gmail.com>
2021-02-13 13:35:45 -0800

    update add firebase configs

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-cyuJcQIX96w3BYa8sSd7j4Lqt20reYk",
+  authDomain: "treehacks-951cf.firebaseapp.com",
+  projectId: "treehacks-951cf",
+  storageBucket: "treehacks-951cf.appspot.com",
+  messagingSenderId: "1082749061729",
+  appId: "1:1082749061729:web:d9d7c3b891c8d327d83820"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export { db };
