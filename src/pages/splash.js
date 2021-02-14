9afc2763e74f1837f99380627d93d03352f5d7c7
Grace Jiang <gracjiang@gmail.com>
2021-02-14 01:39:31 -0800

    updated splash

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -6,21 +6,27 @@ import { Button } from 'evergreen-ui'
 
 import './styles/splash.css'
 
-
+import bgImg from '../assets/splash-bg.png'
+import duckImg from '../assets/duck.png'
 
 export default function SplashPage() {
 
   return (
     <Layout>
-        <div id="splash-page">
+        <div id="splash-page" style={{ 
+            backgroundImage: `url(${bgImg})`,
+            backgroundSize: 'cover'
+        }}>
         <FadeIn>
+            <img src={duckImg} style={{height: '200px'}}/>
             <h1>Study Ducky</h1>
             <h2>Note sharing made easy.</h2>
-            <a href="/explore">
+            <a href="/explore" style={{textDecoration:'none'}}>
                 <Button height={40} marginTop={20}>
                     Get Started
                 </Button>
             </a>
+
            
         </FadeIn>
         </div>
