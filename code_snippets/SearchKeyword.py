b72b1450996fc6f711f78019b7ddc01c75804922
DrakeLin <dragonforest29@berkely.edu>
2021-02-13 16:14:49 -0800

    filter sort

--- code_snippets/SearchKeyword.py
+++ code_snippets/SearchKeyword.py
@@ -1,19 +1,35 @@
 import json
 from collections import OrderedDict
 
+def searchKeyword(word, filt={}):
+    sortedList = []
+    
+    for note in data:
+        if ("courseLevel" in filt) and (note["courseLevel"] not in filt["courseLevel"]):
+            continue
+
+        if ("school" in filt) and (note["school"] not in filt["school"]):
+            continue
+
+        keywords = note["keywords"] #returns an array of keywords
+        if word in keywords:
+            sortedList.append(note)
+
+    sortedList.sort(reverse = True, key= lambda x: x["preference"])
+    return sortedList
+
+# test
+
+def printDicList(lst):
+    temp = []
+    for d in lst:
+        temp.append(d["documentID"])
+    print(temp)
+
 # Opening JSON file 
 with open("test.json") as json_file: 
     data = json.load(json_file) 
 
-sortedList = []
-word = "electron"
-
-for note in data:
-    keywords = note["keywords"] #returns an array of keywords
-    if word in keywords:
-        sortedList.append(note)
-
-sortedList.sort(reverse = True, key= lambda x: x["preference"])
-print(sortedList)
-
+printDicList(searchKeyword("electron",{}))
+printDicList(searchKeyword("matrix"))
 
