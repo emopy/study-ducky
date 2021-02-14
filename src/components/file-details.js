2e859475161eb5907bc00875d3e0ddcc7eb7a863
Grace Jiang <gracjiang@gmail.com>
2021-02-14 03:51:25 -0800

    added file detail viewer

---
+++ src/components/file-details.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './styles/file-details.css'
+
+class FileDetails extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Title {this.props.title}</p>
+                <p>School {this.props.scohol}</p>
+                <p>Description {this.props.description}</p>
+                <p>Keywords {this.props.keywords}</p>
+                <p>URL {this.props.url}</p>
+            </div>
+        );
+    }
+}
+
+export default FileDetails;
---
+++ src/pages/details.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Container from '../components/container'
+import Layout from '../templates/layout'
+import Emoji from '../components/emoji'
+import Navbar from '../components/navbar'
+import FileDetails from '../components/file-details'
+
+import FadeIn from 'react-fade-in';
+import bgImg from '../assets/bg-2.png'
+
+import './styles/details.css'
+
+export default function Details() {
+
+    return (
+        <Layout>
+            <div id="uploadPage" style={{
+                backgroundImage: `url(${bgImg})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                margin: '0px',
+            }}
+            >
+            <Navbar/>
+            <Container><FadeIn>
+
+            <h1>Details <Emoji symbol="✏️"/></h1><br/>
+
+
+            <FileDetails
+                title="title"
+                school="school"
+                description="description"
+                keywords="keywords"
+                url="url"
+
+            />
+            
+            </FadeIn></Container>
+            </div>
+        </Layout>
+    );
+    
+}
+
